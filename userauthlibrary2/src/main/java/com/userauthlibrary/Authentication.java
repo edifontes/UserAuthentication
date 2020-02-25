5e1787e1c0c7bc157a5d3b6e80a2fe4f3e06caf3
edifontes <testarti001@gmail.com>
2020-02-25 07:39:36 -0300

    Initial commit

---
+++ userauthlibrary2/src/main/java/com/userauthlibrary/Authentication.java
@@ -0,0 +1,34 @@
+package com.userauthlibrary;
+
+public class Authentication {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Authentication() {
+    }
+
+    public String getFirstName() {
+        return "Edivan";
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return "Fontes";
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return 43;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
